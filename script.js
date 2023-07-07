9205258da90face6fc3fca5eac03bbcf6ccafde3
adAstra97 <131522998+adAstra97@users.noreply.github.com>
2023-07-07 16:14:26 +0300

    add project

---
+++ script.js
@@ -0,0 +1,37 @@
+let upBtn = document.querySelector('.up-button');
+let downBtn = document.querySelector('.down-button');
+
+let sidebar = document.querySelector('.sidebar');
+let mainSlide = document.querySelector('.main-slide');
+let slidesCount = mainSlide.children.length;
+
+let activeSlideIndex = 0;
+
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
+
+
+upBtn.addEventListener("click", () => {
+   changeSlide('up');
+});
+
+downBtn.addEventListener("click", () => {
+   changeSlide('down');
+});
+
+function changeSlide(direction) {
+   if (direction === "up") {
+      activeSlideIndex++;
+
+      if (activeSlideIndex === slidesCount) {
+         activeSlideIndex = 0;
+      }
+   } else if (direction === 'down') {
+      activeSlideIndex--;
+      if (activeSlideIndex < 0) {
+         activeSlideIndex = slidesCount - 1;
+      }
+   }
+
+   mainSlide.style.transform = `translateY(-${activeSlideIndex * 100}vh)`;
+   sidebar.style.transform = `translateY(${activeSlideIndex * 100}vh)`;
+}
